44d06e4ee009a963e43d2b7ede8995c0b1665436
Pedro Arthur Mesquita Furtado Damas <pedro0961@DIGITALCOLLEGE.LOCAL>
2024-06-22 11:53:44 -0300

    salvarinform

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
+
+function Header({children}) {
+  return (
+    <header className="bg-blue-800">
+      <nav className="mx-auto flex items-center p-6 shadow-md">
+      {children}
+      </nav>
+    </header>
+  )
+}
+
+function HeaderLink({href, title, children}) {
+  return (
+    <a href={href} className="font-semibold leading-6 ml-5 text-white">{title}{children}</a>
+   )
+}
 
 function App() {
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+   return (
+    <>
+    <Header>
+      <HeaderLink href="https://company" title="Company"/>
+        <HeaderLink href="https://marketplace" title="marketplace"/>
+        <HeaderLink href="https://marketplace">lojão</HeaderLink>
+    </Header>
+    </>
+      
   )
 }
 
